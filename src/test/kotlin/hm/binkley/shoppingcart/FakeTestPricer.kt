01a4344ceb6c43a3bcaba1cd9b6164fda4e3e025
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2024-08-07 11:03:03 -0500

    Tests get tree fiddy

--- src/test/kotlin/hm/binkley/shoppingcart/FakeTestPricer.kt
+++ src/test/kotlin/hm/binkley/shoppingcart/FakeTestPricer.kt
@@ -3,5 +3,5 @@ package hm.binkley.shoppingcart
 import org.javamoney.moneta.Money
 
 internal class FakeTestPricer : Pricer {
-    override fun price(item: ShoppingCartItem) = Money.of(10.00, "USD")
+    override fun price(item: ShoppingCartItem) = Money.of(3.50, "USD")
 }
