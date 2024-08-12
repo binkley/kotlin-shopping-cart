b7a39fe58262215e9f738957a3c8333f11812cd8
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2024-08-12 11:21:55 -0500

    Docs

--- src/test/kotlin/hm/binkley/shoppingcart/DescribingTest.kt
+++ src/test/kotlin/hm/binkley/shoppingcart/DescribingTest.kt
@@ -8,6 +8,11 @@ import org.junit.jupiter.api.Test
 private val FLOOR_WAX_UPC_CODE = "012345678905"
 private val DIFFERENT_UPC_CODE = "065100004327"
 
+/**
+ * One does not strictly need this test class&mdash;it is testing a class only
+ * used for other tests&mdash;but it helps to see sources of test problems, and
+ * this test class make those more explicit.
+ */
 internal class DescribingTest {
     private val describer = FakeTestDescriber()
 
