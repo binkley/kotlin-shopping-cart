fa05b70573c970c2b1ba74499575b19b2f5bcdc2
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2024-08-12 11:13:50 -0500

    Test the pricing test fake for testing

--- src/test/kotlin/hm/binkley/shoppingcart/FakeTestPricer.kt
+++ src/test/kotlin/hm/binkley/shoppingcart/FakeTestPricer.kt
@@ -2,6 +2,13 @@ package hm.binkley.shoppingcart
 
 import org.javamoney.moneta.Money
 
+private val FLOOR_WAX_UPC_CODE = "012345678905"
+
 internal class FakeTestPricer : Pricer {
-    override fun price(item: ShoppingCartItem) = Money.of(3.50, "USD")
+    override fun price(item: ShoppingCartItem): Money {
+        require(UPC(FLOOR_WAX_UPC_CODE) == item.upc) {
+            "Unknown UPC for tests: ${item.upc}"
+        }
+        return Money.of(3.50, "USD") // Everything is tree fiddy
+    }
 }
---
+++ src/test/kotlin/hm/binkley/shoppingcart/PricingTest.kt
@@ -0,0 +1,32 @@
+package hm.binkley.shoppingcart
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
+import org.javamoney.moneta.Money
+import org.junit.jupiter.api.Test
+
+private val FLOOR_WAX_UPC_CODE = "012345678905"
+private val DIFFERENT_UPC_CODE = "065100004327"
+
+internal class PricingTest {
+    private val pricer = FakeTestPricer()
+
+    @Test
+    fun `should price items`() {
+        val item = ShoppingCartItem(upc = UPC(FLOOR_WAX_UPC_CODE))
+
+        pricer.price(item) shouldBe Money.of(3.50, "USD")
+    }
+
+    @Test
+    fun `should complain about unknown items`() {
+        val unknownItem = ShoppingCartItem(upc = UPC(DIFFERENT_UPC_CODE))
+
+        val e =
+            shouldThrow<IllegalArgumentException> {
+                pricer.price(unknownItem)
+            }
+        e.message.shouldContain(DIFFERENT_UPC_CODE)
+    }
+}
